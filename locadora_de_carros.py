abe7179b5a6fc638589ec4669b49c7969a59f89f
JESUS <marcos.jesus.py25@gmail.com>
2025-05-29 17:50:55 -0300

    Add files via upload

---
+++ locadora_de_carros.py
@@ -0,0 +1,58 @@
+def trancos( ):
+     print('-' * 50)
+
+trancos()
+print (" aluguel  de carros com a maior frota do brasil| localiza ")
+trancos()
+
+
+
+trancos()
+print("oi qual o  seu  nome ?")
+trancos()
+
+trancos()
+nome=input("digite seu nome ")
+trancos()
+
+trancos()
+print(f"Ola {nome}! estou felizes em  tê-lo conosco")
+trancos()
+
+print("selecione o carro que  deseja alugar :")
+
+def mostra_opcoes():
+     print("1 - BMW")
+     print("2 - MUSTANG")
+     print("3 - BH20")
+     print("4 - FUSCA")
+     print("5 - CIVIC")
+     print("0 - SAIR")
+
+mostra_opcoes()
+
+trancos()
+
+
+
+este_carro =int(input("digite o carro que desejar alugar:"))
+
+match  este_carro:
+     case 1:
+      print(' BMW,boa ideia ele é 1.6 thp - R$ 3M')
+     case 2:
+      print('MUSTANG,boa ideia ele é 1.6 ecoboost - R$ 6M')
+     case 3:
+      print(' BH20,boa ideia ele é 1.6 thp - R$ 200k')
+     case 4:
+      print('FUSCA,boa ideia ele é 1.6 ecoboost - R$ 12k')
+     case 5:
+      print('CIVIC,boa ideia ele é 1.6 ecoboost - R$ 500k')
+     case 0:
+      print("saindo do programa...")
+     case _:
+         print("codigo invalido.por favor, selecione um código do menu.")
+
+
+trancos()
+
